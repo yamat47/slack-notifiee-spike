08010690f53f0063dac9f35b46b61f2509604eb2
Yamaguchi Takuya <yamat47.thirddown@gmail.com>
2022-02-20 17:08:00 +0900

    Update.

--- notify.rb
+++ notify.rb
@@ -7,11 +7,30 @@ require 'slack-notifier'
 require 'slack_notifiee'
 require 'dotenv/load'
 
-SlackNotifiee.enable
+# SlackNotifiee.enable
 
 webhook_url = ENV.fetch('SLACK_WEBHOOK_URL')
 notifier = Slack::Notifier.new(webhook_url)
 
-notifier.post text: 'Hello, World!', at: 'here', channel: '#random'
+# notifier.post text: 'Hello, World!', at: 'here', channel: '#general'
+# notifier.ping 'Hello, World!'
+# notifier.post text: "feeling spooky", icon_emoji: ":ghost:"
 
-pp SlackNotifiee.notifications
+# notifier.post blocks: [
+#   {"type": "section", "text": {"type": "plain_text", "text": "Hello world"}},
+#   {"type": "section", "text": {"type": "plain_text", "text": "Hello world"}},
+#   {"type": "section", "text": {"type": "plain_text", "text": "Hello world"}},
+#   {"type": "section", "text": {"type": "plain_text", "text": "Hello world"}},
+# ], username: 'notify', icon_emoji: ':smile:'
+
+notifier.post(
+  username: 'Notificaion bot via slack-notify',
+  channel: '#general',
+  attachments: [{"pretext": "pre-hello", "text": "text-world"}],
+  blocks: [{"type": "section", "text": {"type": "plain_text", "text": "Hello world"}}],
+  text: 'Hello world',
+  icon_emoji: ':chart_with_upwards_trend:',
+  icon_url: 'https://picsum.photos/200/300'
+)
+
+# SlackNotifiee.notifications
